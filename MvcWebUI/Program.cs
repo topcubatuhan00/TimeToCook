f4eea508b295c9e27675e721f4c029f737377025
batuhan topcu <topcubatuhan00@gmail.com>
2023-06-29 17:07:58 +0300

    first commit

---
+++ MvcWebUI/Program.cs
@@ -0,0 +1,45 @@
+using Business.Abstract;
+using Business.Concretes;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using MvcWebUI.Helpers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IProductService, ProductManager>();
+builder.Services.AddSingleton<ICategoryService, CategoryManager>();
+builder.Services.AddSingleton<IProductDal, EfProductDal>();
+builder.Services.AddSingleton<ICategoryDal, EfCategoryDal>();
+
+builder.Services.AddScoped<ICartService, CartManager>();
+builder.Services.AddScoped<ICartSessionHelper, CartSessionHelper>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
