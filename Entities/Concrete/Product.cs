73030140b78cf5c156013705ecad3b25399fc1a1
batuhan topcu <topcubatuhan00@gmail.com>
2023-07-01 22:17:23 +0300

    code cleanup

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -1,14 +1,11 @@
-﻿using Core.Entities.Abstract;
+﻿namespace Entities.Concrete;
 
-namespace Entities.Concrete
+public class Product
 {
-    public class Product : IEntity
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Photo { get; set; }
-        public string CreatedDate { get; set; }
-        public string Amount { get; set; }
-        public string SKTDate { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Photo { get; set; }
+    public string CreatedDate { get; set; }
+    public string Amount { get; set; }
+    public string SKTDate { get; set; }
 }
