3c126c2916debf565fbb41ee520ac689a28de8c4
Batuhan Topçu <72348346+topcubatuhan00@users.noreply.github.com>
2023-07-07 22:55:50 +0300

    kategorileri taşıdım

--- MvcWebUI/Program.cs
+++ MvcWebUI/Program.cs
@@ -12,6 +12,8 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // dsafsdfsdf
+    // dsfsdfsdfsdf
     app.UseHsts();
 }
 
